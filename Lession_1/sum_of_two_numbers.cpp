8cfad31fecb10f6334d33ee5803e9ececce954c7
Satish Yadav <sansat.dev@gmail.com>
2024-12-08 00:50:50 +0530

    Addition of two numbers

---
+++ Lession_1/sum_of_two_numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+	int number1 = 50; //first number is assigned with value 50
+	int number2 = 100; //second number is assigend with value 100
+	int total = number1 + number2;
+	std::cout << "The sum of  " << number1  << " and " << number2 << " is " << total << std::endl;
+	return 0;
+}
