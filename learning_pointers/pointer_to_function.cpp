88d5a63217f2dd6ef6ebea9e3d70e5c92f625695
Satish Yadav <sansat.dev@gmail.com>
2024-12-23 19:10:39 +0530

    Added Comments

--- learning_pointers/pointer_to_function.cpp
+++ learning_pointers/pointer_to_function.cpp
@@ -4,9 +4,9 @@ int addition();
 
 int main(){
 	int result;
-	int (*ptr)();
-	ptr = &addition;
-	result = (*ptr)();
+	int (*ptr)(); //initilize a pointer functino 
+	ptr = &addition;  //storing the address of funtion addtion
+	result = (*ptr)(); //calling the fucntion and storing the return value into result 
 	std::cout << "The sun is :: " << result << std::endl;
 	return 0;
 }
